e04630205b964dc6d5fb134df19f46c92b6119ff
Adhinesh-Reddy <68197578+Adhinesh-Reddy@users.noreply.github.com>
2025-10-20 14:23:23 -0400

    Add files via upload

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,7 +29,7 @@ const Hero = () => {
                     />
 
                     <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-                        Hi, I&apos;m Sai, a Full Stack Developer.
+                        Hi, I&apos;m Sai, a Software Engineer.
                     </p>
 
                     <a href="#experience">
